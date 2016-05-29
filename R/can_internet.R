15e57345c6ae119e1b0e3bf40449d2fe964ad0fc
Frojast <rsimon64@gmail.com>
2016-05-29 06:20:57 -0500

    parameter to can_internet

--- R/can_internet.R
+++ R/can_internet.R
@@ -3,9 +3,10 @@
 #'
 #' Check for internet connection.
 #'
+#' @param url a url to check; default is Google
 #' @references stackoverflow
 #' @return logical TRUE or FALSE
 #' @export
-can_internet <- function(){
-  !is.null(curl::nslookup("www.google.org", error = FALSE))
+can_internet <- function(url = "www.google.org"){
+  !is.null(curl::nslookup(url, error = FALSE))
 }
