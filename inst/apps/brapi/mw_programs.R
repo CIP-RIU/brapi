05b7ce300fb331a96f3b4190801520bb4ee8de01
Frojast <rsimon64@gmail.com>
2016-12-01 16:55:18 -0500

    refactoring

--- inst/apps/brapi/mw_programs.R
+++ inst/apps/brapi/mw_programs.R
@@ -1,6 +1,7 @@
 library(jug)
 library(jsonlite)
 source(system.file("apps/brapi/utils/brapi_status.R", package = "brapi"))
+source(system.file("apps/brapi/utils/paging.R", package = "brapi"))
 
 programs_data = tryCatch({
   read.csv(system.file("apps/brapi/data/programs.csv", package = "brapi"), stringsAsFactors = FALSE)
@@ -9,23 +10,27 @@ programs_data = tryCatch({
 }
 )
 
-prg_list = function(abbr = NULL, prg = NULL){
+programs_list = function(abbr = "none", prg = "none", page=0, pageSize = 10){
   if(is.null(programs_data)) return(NULL)
-  if(!is.null(abbr)){
+  if(abbr != "none"){
     programs_data = programs_data[programs_data$abbreviation == abbr, ]
     if(nrow(programs_data) == 0) return(NULL)
   }
 
-  if(!is.null(prg)){
+  if(prg != "none") {
     programs_data = programs_data[programs_data$name == prg, ]
     if(nrow(programs_data) == 0) return(NULL)
   }
+  # paging here after filtering
+  pg = paging(programs_data, page, pageSize)
+  programs_data <- programs_data[pg$recStart:pg$recEnd, ]
 
   n = nrow(programs_data)
   out = list(n)
   for(i in 1:n){
     out[[i]] <- as.list(programs_data[i, ])
   }
+  attr(out, "pagination") = pg$pagination
   out
 }
 
@@ -33,7 +38,7 @@ prg_list = function(abbr = NULL, prg = NULL){
 programs = list(
   metadata = list(
     pagination = list(
-      pageSize = 100,
+      pageSize = 10,
       currentPage = 0,
       totalCount = nrow(programs_data),
       totalPages = 1
@@ -41,29 +46,25 @@ programs = list(
     status = NULL,
     datafiles = list()
   ),
-  result = list(data = prg_list())
+  result = list(data = programs_list())
 )
 
 
 process_programs <- function(req, res, err){
   prms <- names(req$params)
-  if('abbreviation' %in% prms){
-    programs$result$data = prg_list(req$params$abbreviation)
-  }
-  if('programName' %in% prms){
-    programs$result$data = prg_list(prg = req$params$programName)
-  }
+  page = ifelse('page' %in% prms, as.integer(req$params$page), 0)
+  pageSize = ifelse('pageSize' %in% prms, as.integer(req$params$pageSize), 10)
+  abbreviation = ifelse('abbreviation' %in% prms, req$params$abbreviation, "none")
+  programName = ifelse('programName' %in% prms, req$params$programName, "none")
 
-  if('page' %in% prms | 'pageSize' %in% prms){
-   programs$metadata <- brapi_status(code = 200,
-          "Parameters 'page' and 'pageSize' are not implemented." )
-  }
-
+  programs$result$data = programs_list(abbreviation, programName, page, pageSize)
+  programs$metadata$pagination = attr(programs$result$data, "pagination")
 
   if(is.null(programs$result$data)){
     res$set_status(404)
     programs$metadata <- brapi_status(100, "No matching results!")
   }
+  res$set_header("Access-Control-Allow-Methods", "GET")
   res$json(programs)
 
 }
