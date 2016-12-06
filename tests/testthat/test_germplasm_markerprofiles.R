11d834e2f9782c932625a555ab60be7fe3c4e8c9
Frojast <rsimon64@gmail.com>
2016-12-06 15:51:59 -0500

    updating tests

--- tests/testthat/test_germplasm_markerprofiles.R
+++ tests/testthat/test_germplasm_markerprofiles.R
@@ -5,8 +5,21 @@ if (check_server_status == 200) {
 context("Testing the path 'germplasm/{id}/markerprofiles'")
 
 
-test_that("Calls are listed.", {
-  expect_equal(length(brapi::germplasm_markerprofiles()), 2)
+test_that("Basic return object is ok.", {
+  acall = brapi::germplasm_markerprofiles()
+  expect_equal(length(acall), 2)
+  expect_equal(names(acall)[1], "metadata")
+  expect_equal(names(acall)[2], "result")
+})
+
+test_that("Metadata object is ok.", {
+  metadata = brapi::germplasm_markerprofiles()$metadata
+  expect_equal(names(metadata)[1], "pagination")
+  expect_equal(names(metadata)[2], "status")
+  expect_equal(names(metadata)[3], "data")
+  expect_equal(capture.output(metadata$pagination), "named list()")
+  expect_equal(class(metadata[2]), "list")
+  expect_equal(class(metadata[3]), "list")
 })
 
 test_that("Parameters are tested.", {
