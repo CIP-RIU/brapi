7c05221851648e752ef1e36b03ae9bc820455579
c5sire <rsimon64@gmail.com>
2016-12-07 20:50:20 -0500

    Corrected testing without server.

--- tests/testthat/check_server_status.R
+++ tests/testthat/check_server_status.R
@@ -1,7 +1,7 @@
 check_server_status <- tryCatch({
   curl::curl_fetch_memory('http://127.0.0.1:2021/brapi/v1/')$status_code
+  show_info(FALSE)
+  connect()
 }, error = function(e){
   555
 })
-show_info(FALSE)
-connect()
