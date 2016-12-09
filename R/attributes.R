e6c7f046cd3d7e36ec0fa3750b5aa4afd62ca709
Frojast <rsimon64@gmail.com>
2016-12-09 09:24:30 -0500

    Implemented attributes call

---
+++ R/attributes.R
@@ -0,0 +1,30 @@
+#' attributes
+#'
+#' attibutes call.
+#'
+#' @param attributeCategoryDbId
+#'
+#' @return rclass as set by parameter
+#' @import httr
+#' @author Reinhard Simon
+#' @references \url{http://docs.brapi.apiary.io/#reference/0/list-attributes-available/attributes-by-attributecategorydbid}
+#' @export
+attributes <- function(attributeCategoryDbId = 0, rclass = "tibble"){
+  brapi::check(FALSE)
+  brp <- get_brapi()
+  attributes_list = paste0(brp, "attributes/?attributeCategoryDbId=", attributeCategoryDbId)
+
+
+  tryCatch({
+    res <- brapiGET(attributes_list)
+    res <- httr::content(res, "text", encoding = "UTF-8")
+
+    out <- dat2tbl(res, rclass)
+    if(rclass %in% c("tibble", "data.frame")){
+      out$values <- sapply(out$values, paste, collapse = "; ")
+    }
+    out
+  }, error = function(e){
+    NULL
+  })
+}
--- tests/testthat/check_server_status.R
+++ tests/testthat/check_server_status.R
@@ -1,7 +1,8 @@
 check_server_status <- tryCatch({
   curl::curl_fetch_memory('http://127.0.0.1:2021/brapi/v1/')$status_code
   show_info(FALSE)
-  connect()
+  ok <- connect()
+  200
 }, error = function(e){
   555
 })
