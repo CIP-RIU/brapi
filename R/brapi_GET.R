ef29e6e2667035a13cabfa086f38f1809e199024
Frojast <rsimon64@gmail.com>
2016-12-09 14:28:46 -0500

    u

--- R/brapi_GET.R
+++ R/brapi_GET.R
@@ -2,5 +2,12 @@ brapiGET <- function(url){
   res <- httr::GET(url)
   txt <- ifelse(res$status == 200, " (ok)!", " (error)!" )
   message_brapi(paste0("Server status: ", res$status, txt, "\n"))
+
+  # TODO: Insert here status messages if any!
+  out <- httr::content(res, "text", encoding = "UTF-8")
+  # Get JSON
+  # Check if status object has any key-value pairs
+  # if so: cycle through and print a message for each!
+
   res
 }
