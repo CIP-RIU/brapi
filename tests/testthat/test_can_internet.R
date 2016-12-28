a2e197ba470308276ffcc944eb58f2b9f819dc47
c5sire <c5sire@gmail.com>
2016-12-28 11:33:16 -0500

    test for can_internet

---
+++ tests/testthat/test_can_internet.R
@@ -0,0 +1,12 @@
+source("check_server_status.R")
+
+if (check_server_status == 200) {
+
+context("Testing 'can_internet'")
+
+test_that("Can internet parameters work.", {
+  expect_equal(can_internet(), TRUE)
+
+})
+
+}
