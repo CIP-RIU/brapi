b7c482326bee64189d855459a9ae1f8a433b174a
Frojast <rsimon64@gmail.com>
2017-01-06 13:42:38 -0500

    update tutorial

--- inst/doc/tutorial.R
+++ inst/doc/tutorial.R
@@ -1,1 +1,21 @@
+## ----eval=FALSE----------------------------------------------------------
+#  
+#  brapi::mock_server()
+#  
+
+## ----eval=FALSE----------------------------------------------------------
+#  
+#  R -e "brapi::mock_server()"
+#  
+
+## ----eval=FALSE----------------------------------------------------------
+#  library(brapi)
+#  
+#  connect() # the standard parameter will work connect to a running local mock server
+#  
+#  crops() # which crops are available
+#  
+#  calls() # which calls are available
+#  
+#  
 
