b9e0c245e43500fb3ffb2ca5ff437dea09c36482
c5sire <rsimon64@gmail.com>
2017-01-18 07:58:29 -0800

    Added simple map charts

--- R/chart-methods.R
+++ R/chart-methods.R
@@ -1,6 +1,6 @@
 #' chart.brapi_locations
 #'
-#' print method for an object of class brapi_con, which will only display the crop,  database address:port and user
+#' print method for an object of class brapi_locations, which will only display the crop,  database address:port and user
 #'
 #' @param x a brapi_locations data.frame/tibble or a numeric vector of longitudes
 #' @param ... other plot parameters
@@ -9,7 +9,7 @@
 #' @export
 chart.brapi_locations <- function(x, ...) {
     prms <- list(...)
-    #message(paste(names(prms)))
+
     chart_type <- ifelse(!is.null(prms$chart_type),
                          prms$chart_type,
                          "plot")
@@ -33,7 +33,7 @@ chart.brapi_locations <- function(x, ...) {
 
         xr <- range(with_geo$longitude)
         yr <- range(with_geo$latitude)
-        #requireNamespace("maps")
+
         maps::map("world", xlim = xr, ylim = yr)
         graphics::title(paste0("Locations from database"))
         graphics::points(x = with_geo$longitude, y = with_geo$latitude, col = "red")
@@ -43,5 +43,74 @@ chart.brapi_locations <- function(x, ...) {
     }
    }
 
+
+}
+
+
+#' chart.brapi_genomemaps
+#'
+#' print method for an object of class brapi_brapi_genomemaps, which will only display the crop,  database address:port and user
+#'
+#' @param x a brapi_locations data.frame/tibble or a numeric vector of longitudes
+#' @param ... other plot parameters
+#'
+#' @author Reinhard Simon
+#' @export
+chart.brapi_genomemaps <- function(x, ...) {
+  prms <- list(...)
+  stopifnot(!is.null(x$markerCount & !is.null(x$mapDbId)))
+  chart_type <- ifelse(!is.null(prms$chart_type),
+                       prms$chart_type,
+                       "plot")
+
+
+  if (chart_type == "plot") {
+    ttl <- paste0("Comparative view of genome maps")
+    graphics::barplot(x$markerCount,
+                      horiz = T,
+                      axisnames = T,
+                      names.arg =  x$mapDbId,
+                      las = 1,
+                      main = ttl,
+                      xlab = "Number of markers")
+  }
+
+  if (chart_type == "map") {
+  }
+
+}
+
+
+
+#' chart.brapi_genomemaps_details
+#'
+#' print method for an object of class brapi_brapi_genomemaps_details, which will only display the crop,  database address:port and user
+#'
+#' @param x a brapi_genomemaps_details_data data.frame/tibble or a numeric vector of longitudes
+#' @param ... other plot parameters
+#'
+#' @author Reinhard Simon
+#' @export
+chart.brapi_genomemaps_details <- function(x, ...) {
+  prms <- list(...)
+  #stopifnot(!is.null(x$linkageGroupId & !is.null(x$maxPosition)))
+  chart_type <- ifelse(!is.null(prms$chart_type),
+                       prms$chart_type,
+                       "plot")
+
+
+  if (chart_type == "plot") {
+    ttl <- paste0("Linkage groups")
+    graphics::barplot(x$maxPosition,
+                      horiz = T,
+                      axisnames = T,
+                      names.arg =  x$linkageGroupId,
+                      las = 1,
+                      main = ttl,
+                      xlab = "Maximum length of linkage group")
+  }
+
+  if (chart_type == "map") {
+  }
 
 }
