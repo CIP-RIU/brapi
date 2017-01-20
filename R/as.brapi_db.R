2ea1538029517350991e596719bb9c815889ed43
mverouden <m.p.h.verouden@gmail.com>
2017-01-20 12:43:53 +0100

    corrected double secure=secure in as.brapi_db and updated readability in *brapi_db.R

--- R/as.brapi_db.R
+++ R/as.brapi_db.R
@@ -1,30 +1,49 @@
-#' Tas.brapi_db
+#' as.brapi_db
 #'
-#' @param crop character
+#' @param secure logical; default: false
 #' @param protocol character
 #' @param db character
-#' @param secure logical; default: false
+#' @param port integer; default: 80
 #' @param apipath character; default: ''
 #' @param multicrop logical; default: false
+#' @param crop character
 #' @param user character; default: ''
 #' @param password character; default: ''
 #' @param token character; default: ''
 #' @param granttype character; default: 'password'
 #' @param clientid character; default: 'rbrapi'
 #' @param bms logical; default: false
-#' @param port integer; default: 80
 #'
 #' @return brapi_db class
 #' @export
-as.brapi_db <- function(
-  secure = FALSE, protocol = "http://", db = "127.0.0.1", port = 80,
-  apipath = NULL,
-  multicrop = FALSE, crop = "", user = "", password = "", token = "",
-  granttype = "password", clientid = "rbrapi", bms = FALSE) {
-    out <- list(secure = secure, secure = secure, db = db, port = port,
-                apipath = apipath, multicrop = multicrop,
-                crop = crop, user = user, password = password, token = token,
-                granttype = granttype, clientid = clientid, bms = bms)
-    class(out) <- c("list", "brapi_db", "brapi", "brapi_con")
+as.brapi_db <- function(secure = FALSE,
+                        protocol = "http://",
+                        db = "127.0.0.1",
+                        port = 80,
+                        apipath = NULL,
+                        multicrop = FALSE,
+                        crop = "",
+                        user = "",
+                        password = "",
+                        token = "",
+                        granttype = "password",
+                        clientid = "rbrapi",
+                        bms = FALSE) {
+  out <- list(secure = secure,
+              db = db,
+              port = port,
+              apipath = apipath,
+              multicrop = multicrop,
+              crop = crop,
+              user = user,
+              password = password,
+              token = token,
+              granttype = granttype,
+              clientid = clientid,
+              bms = bms)
+    class(out) <- c("list",
+                    "brapi_db",
+                    "brapi",
+                    "brapi_con")
     out
 }
