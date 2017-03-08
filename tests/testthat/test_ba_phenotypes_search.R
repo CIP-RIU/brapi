a1bcc70cf80571e98d9bf9e1a8812fca9322303e
Frojast <rsimon64@gmail.com>
2017-03-08 10:28:16 -0500

    u test

---
+++ tests/testthat/test_ba_phenotypes_search.R
@@ -0,0 +1,28 @@
+source("check_server_status.R")
+
+if (check_server_status == 200) {
+
+context("Testing the path 'phenotype-search'")
+
+  con <- ba_connect(secure = FALSE)
+
+test_that("Basics.", {
+  expect_equal(length(ba_phenotypes_search(con, "1", rclass = "list")), 2)
+  #expect_equal(nrow(ba_phenotypes_search(con, "1", rclass = "data.frame")), 3)
+})
+
+test_that("Parameters", {
+  # expect_equal(nrow(ba_phenotypes_search(con, "1")), 3)
+  # expect_equal(nrow(ba_phenotypes_search(con, "2")), 4)
+})
+
+
+test_that("Classes", {
+  expect_equal("json" %in% class(ba_phenotypes_search(con, "1", rclass = "json")), TRUE)
+  expect_equal("list" %in% class(ba_phenotypes_search(con, "1", rclass = "list")), TRUE)
+  # expect_equal("data.frame" %in% class(ba_phenotypes_search(con, rclass = "data.frame")), TRUE)
+  # expect_equal("tbl_df" %in% class(ba_phenotypes_search(con)), TRUE)
+  expect_equal("ba_phenotypes_search" %in% class(ba_phenotypes_search(con, "1", rclass = "json")), TRUE)
+})
+
+}
