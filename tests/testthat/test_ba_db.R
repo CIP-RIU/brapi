795d8d548ba7d37ca435c92b2ff474cab497a511
c5sire <c5sire@gmail.com>
2017-04-25 21:54:41 -0500

    u

--- tests/testthat/test_ba_db.R
+++ tests/testthat/test_ba_db.R
@@ -6,8 +6,8 @@ context("Testing the helper function ba_db")
 
 test_that("Calls are listed.", {
   expect_equal("ba_db_list" %in% class(ba_db()), TRUE)
-  expect_equal(length(ba_db()), 13)
-  expect_equal(names(ba_db()[7]), "mockbase")
+  expect_equal(length(ba_db()), 14)
+  expect_equal(names(ba_db()[8]), "mockbase")
   expect_equal(length(ba_db()$mockbase), 15)
 })
 
