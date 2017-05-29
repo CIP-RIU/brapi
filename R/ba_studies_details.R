9974909058728d09f3db1675546c52df223c084a
mverouden <m.p.h.verouden@gmail.com>
2017-05-29 16:16:56 +0200

    progression clean up 29/05/2017

--- R/ba_studies_details.R
+++ R/ba_studies_details.R
@@ -14,23 +14,27 @@
 #' @family studies
 #' @family phenotyping
 #' @export
-ba_studies_details <- function(con = NULL, studiesDbId = "0", rclass = "tibble") {
-    ba_check(con, FALSE, "studies/id")
-    stopifnot(is.character(studiesDbId))
-    check_rclass(rclass)
-    
-    studies <- paste0(get_brapi(con), "studies/", studiesDbId, "/")
-    try({
-        res <- brapiGET(studies, con = con)
-        out <- NULL
-        if (is.ba_status_ok(res)) {
-            res <- httr::content(res, "text", encoding = "UTF-8")
-            if (rclass %in% c("json", "list")) 
-                out <- dat2tbl(res, rclass)
-            if (rclass %in% c("data.frame", "tibble")) 
-                out <- stdd2tbl(res, rclass)
-            class(out) <- c(class(out), "ba_studies_details")
-        }
-        return(out)
-    })
+ba_studies_details <- function(con = NULL,
+                               studiesDbId = "0",
+                               rclass = "tibble") {
+
+  ba_check(con = con, verbose = FALSE, brapi_calls = "studies/id")
+  stopifnot(is.character(studiesDbId))
+  check_rclass(rclass = rclass)
+  studies <- paste0(get_brapi(brapi = con), "studies/", studiesDbId, "/")
+  try({
+    res <- brapiGET(url = studies, con = con)
+    out <- NULL
+    if (is.ba_status_ok(res)) {
+      res <- httr::content(x = res, as = "text", encoding = "UTF-8")
+      if (rclass %in% c("json", "list")) {
+        out <- dat2tbl(res = res, rclass = rclass)
+      }
+      if (rclass %in% c("data.frame", "tibble")) {
+        out <- stdd2tbl(res = res, rclass = rclass)
+      }
+      class(out) <- c(class(out), "ba_studies_details")
+    }
+    return(out)
+  })
 }
--- R/stdd2tbl.R
+++ R/stdd2tbl.R
@@ -1,47 +1,41 @@
 stdd2tbl <- function(res, rclass) {
-    lst <- jsonlite::fromJSON(res)
-    if (length(lst$result) == 0) 
-        return(NULL)
-    dat <- jsonlite::toJSON(lst$result)
-
-    dat <- jsonlite::fromJSON(dat, simplifyDataFrame = TRUE, flatten = TRUE)
-
-    contacts <- dat$contacts
-    dat$contacts <- NULL
-
-
-    location <- dat$location
-    if(length(location$additionalInfo) == 0) location$additionalInfo <- NULL
-    location <- as.data.frame(location)
-    dat$location <- NULL
-
-    additionalInfo <- dat$additionalInfo
-    additionalInfo <- as.data.frame(additionalInfo)
-    dat$additionalInfo <- NULL
-
-    if(length(location) == 0) {
-      location <- cbind(studyDbId = rep(dat$studyDbId, nrow(location)), location)
-      names(location)[2:ncol(location)] <- paste("location", names(location)[2:ncol(location)], sep = ".")
-      dat <- merge(dat, location)
-      dat$location.studyDbId <- NULL
-    }
-
-    if (length(contacts) != 0) {
-        contacts <- cbind(studyDbId = rep(dat$studyDbId, nrow(contacts)), contacts)
-        names(contacts)[2:ncol(contacts)] <- paste("contacts", names(contacts)[2:ncol(contacts)], sep = ".")
-        dat <- merge(dat, contacts)
-        dat$contacts.studyDbId <- NULL
-    }
-
-    if (length(additionalInfo) != 0) {
-      additionalInfo <- cbind(studyDbId = rep(dat$studyDbId, nrow(additionalInfo)), additionalInfo)
-      names(additionalInfo)[2:ncol(additionalInfo)] <- paste("additionalInfo", names(additionalInfo)[2:ncol(additionalInfo)], sep = ".")
-      dat <- merge(dat, additionalInfo)
-      dat$additionalInfo.studyDbId <- NULL
-    }
-    
-    
-    if (rclass == "tibble") 
-        dat <- tibble::as_tibble(dat)
-    return(dat)
+  lst <- jsonlite::fromJSON(txt = res)
+  if (length(lst$result) == 0) {
+    return(NULL)
+  }
+  dat <- jsonlite::toJSON(x = lst$result)
+  dat <- jsonlite::fromJSON(x = dat, simplifyDataFrame = TRUE, flatten = TRUE)
+  contacts <- dat$contacts
+  dat$contacts <- NULL
+  location <- dat$location
+  if (length(location$additionalInfo) == 0) {
+    location$additionalInfo <- NULL
+  }
+  location <- as.data.frame(x = location)
+  dat$location <- NULL
+  additionalInfo <- dat$additionalInfo
+  additionalInfo <- as.data.frame(x = additionalInfo)
+  dat$additionalInfo <- NULL
+  if(length(location) == 0) {
+    location <- cbind(studyDbId = rep(x = dat$studyDbId, times = nrow(location)), location)
+    names(location)[2:ncol(location)] <- paste("location", names(location)[2:ncol(location)], sep = ".")
+    dat <- merge(x = dat, y = location)
+    dat$location.studyDbId <- NULL
+  }
+  if (length(contacts) != 0) {
+    contacts <- cbind(studyDbId = rep(x = dat$studyDbId, times = nrow(contacts)), contacts)
+    names(contacts)[2:ncol(contacts)] <- paste("contacts", names(contacts)[2:ncol(contacts)], sep = ".")
+    dat <- merge(x = dat, y = contacts)
+    dat$contacts.studyDbId <- NULL
+  }
+  if (length(additionalInfo) != 0) {
+    additionalInfo <- cbind(studyDbId = rep(x = dat$studyDbId, times = nrow(additionalInfo)), additionalInfo)
+    names(additionalInfo)[2:ncol(additionalInfo)] <- paste("additionalInfo", names(additionalInfo)[2:ncol(additionalInfo)], sep = ".")
+    dat <- merge(dat, additionalInfo)
+    dat$additionalInfo.studyDbId <- NULL
+  }
+  if (rclass == "tibble") {
+    dat <- tibble::as_tibble(x = dat)
+  }
+  return(dat)
 }
