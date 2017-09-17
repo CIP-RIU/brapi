fb44a9f1656cfdb85baadabf18c095ff9169c92d
c5sire <c5sire@gmail.com>
2017-09-17 14:23:57 -0500

    Improved console printing of server status. Only print content.

--- R/show_server_status_messages.R
+++ R/show_server_status_messages.R
@@ -1,7 +1,24 @@
 show_server_status_messages <- function(out) {
 
-  ba_message(crayon::blue(out$info))
-  ba_message(crayon::green(out$success))
-  ba_message(crayon::red(out$error))
+  ba_message(crayon::yellow("Status details from Server:"))
+
+  if(all(out$info == "", out$success == "", out$error =="")) {
+    ba_message(crayon::yellow("None."))
+  } else {
+
+    if (out$info != "") {
+      ba_message(crayon::blue("\nInfos"))
+      ba_message(crayon::blue(out$info))
+    }
+    if (out$success != "") {
+      ba_message(crayon::green("\nSuccesses:"))
+      ba_message(crayon::green(out$success))
+    }
+    if (out$error != "") {
+      ba_message(crayon::red("\nErrors:"))
+      ba_message(crayon::red(out$error))
+    }
+
+  }
 
 }
