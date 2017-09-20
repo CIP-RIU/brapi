3d2211ed6d2b1af2c2adf9f4258af2a107c68eb8
mverouden <m.p.h.verouden@gmail.com>
2017-09-20 16:02:14 +0200

    fixed to get programDbId as jstring instead of jnumber

--- R/trl2tbl.R
+++ R/trl2tbl.R
@@ -16,21 +16,20 @@ trl2tbl <- function(res, rclass) {
     tidyjson::enter_object("data") %>%
     tidyjson::gather_array() %>%
     tidyjson::spread_values(
-       programDbId = tidyjson::jnumber("programDbId"),
-       programName = tidyjson::jstring("programName"),
-       trialDbId = tidyjson::jstring("trialDbId"),
-       trialName = tidyjson::jstring("trialName"),
-       active = tidyjson::jstring("active"),
-       additionalInfo = tidyjson::jstring("additionalInfo"),
-       startDate = tidyjson::jstring("startDate"),
-       endDate = tidyjson::jstring("endDate")) %>%
+      programDbId = tidyjson::jstring("programDbId"),
+      programName = tidyjson::jstring("programName"),
+      trialDbId = tidyjson::jstring("trialDbId"),
+      trialName = tidyjson::jstring("trialName"),
+      active = tidyjson::jstring("active"),
+      additionalInfo = tidyjson::jstring("additionalInfo"),
+      startDate = tidyjson::jstring("startDate"),
+      endDate = tidyjson::jstring("endDate")) %>%
     tidyjson::enter_object("studies") %>%
     tidyjson::gather_array() %>%
     tidyjson::spread_values(
       studyDbId = tidyjson::jnumber("studyDbId"),
       studyName = tidyjson::jstring("studyName"),
-      locationDbId = tidyjson::jnumber("locationDbId")
-    ) %>%
+      locationDbId = tidyjson::jnumber("locationDbId")) %>%
     dplyr::select(programDbId,
                   programName,
                   trialDbId,
@@ -42,10 +41,12 @@ trl2tbl <- function(res, rclass) {
                   studyDbId,
                   studyName,
                   locationDbId)
-  out$additionalInfo = sapply(out$additionalInfo, function(x) ifelse(x == "list()", "", paste(x, collapse = ", ")))
-
+  out$additionalInfo <- sapply(X = out$additionalInfo,
+                               FUN = function(x) {
+                                 ifelse(x == "list()", "", paste(x, collapse = ", "))
+                               }
+  )
   # TODO: still find a better way to deal with additionalInfo!
-
   if (rclass == "tibble") {
     out <- tibble::as_tibble(out)
   }
