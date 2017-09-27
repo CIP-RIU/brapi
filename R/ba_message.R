c98f408b80384488fabc2699fa26a7e9b40e84e6
mverouden <m.p.h.verouden@gmail.com>
2017-09-27 16:01:35 +0200

    commented out the if statement and added an explanation as comment

--- R/ba_message.R
+++ R/ba_message.R
@@ -1,7 +1,11 @@
 ba_message <- function(msg = "Using local test server.") {
   stopifnot(is.character(msg))
-
-  if (getOption(x = "brapi_info", default = FALSE)) {
-    return(message(msg))
-  }
+  # # This will never return a message getOption extracts a global option
+  # # getOption(x = "brapi_info") will be NULL unless set via options(...)
+  # # therefore the default = FALSE will almost always break the if function
+  # # and the return function is not executed
+  # if (getOption(x = "brapi_info", default = FALSE)) {
+  #   return(message(msg))
+  # }
+  return(message(msg))
 }
