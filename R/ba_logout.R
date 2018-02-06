8bf57be5e3f66f8ec78ac3923fc2e1c91dfe6cc7
Verouden, Maikel <m.p.h.verouden@gmail.com>
2018-02-06 09:57:33 +0100

    replacements of brapi object into con object

--- R/ba_logout.R
+++ R/ba_logout.R
@@ -15,30 +15,31 @@
 #' @export
 ba_logout <- function(con) {
   stopifnot(is.ba_con(con))
-  brapi <- con
   # Check for internet connection
   ba_can_internet()
   # Set authentication URL
   callpath <- "token"
-  omc <- brapi$multicrop
-  brapi$multicrop <- FALSE
-  callurl <- paste0(get_brapi(brapi = brapi), callpath)
-  brapi$multicrop <- omc
-  dat <- list(access_token = brapi$token)
+  omc <- con$multicrop
+  con$multicrop <- FALSE
+  callurl <- paste0(get_brapi(con = con), callpath)
+  con$multicrop <- omc
+  dat <- list(access_token = con$token)
   ba_message(jsonlite::toJSON(x = dat, pretty = TRUE))
   # obtain response from httr::delete call
-  resp <- httr::DELETE(url = callurl, body = dat, encode = ifelse(brapi$bms == TRUE, "json", "form"))
+  resp <- httr::DELETE(url = callurl,
+                       body = dat,
+                       encode = ifelse(con$bms == TRUE, "json", "form"))
   # Status other than unauthorized
   if (resp$status_code != 201) {
     # Status other than Unauthorized and OK
-    brapi$token <- ""
+    con$token <- ""
     httr::stop_for_status(x = resp)
   } else {
     xout <- httr::content(x = resp)
     ba_message(msg = jsonlite::toJSON(xout, pretty = TRUE))
-    brapi$token <- ""
+    con$token <- ""
     message("Successfully logged out!")
   }
-  ## Return brapi object with
-  return(brapi)
+  ## Return con object with
+  return(con)
 }
