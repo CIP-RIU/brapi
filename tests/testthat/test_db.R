c5ee8d4527bf15bd10fc9c74a33be9edc479db7f
c5sire <rsimon64@gmail.com>
2018-02-08 15:59:59 +0100

    adding tests

---
+++ tests/testthat/test_db.R
@@ -0,0 +1,16 @@
+context("databases")
+
+con <- ba_db()
+
+test_that("DBs are present", {
+
+  expect_that(length(con) == 16, is_true())
+
+})
+
+test_that("DBs contains at least one well-known", {
+
+  expect_that("sweetpotatobase" %in% names(con), is_true())
+
+})
+
