aed27ca82eb9e07ce24c3c31bd323e427809c077
Reinhard Simon <rsimon64@gmail.com>
2018-05-31 11:45:38 -0500

    u

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,7 @@
 library(testthat)
 library(brapi)
 
-testthat::skip_on_cran()
-if(!brapi::ba_can_internet()) skip("Skipping tests since no internet!")
+#testthat::skip_on_cran()
+#if(!brapi::ba_can_internet()) skip("Skipping tests since no internet!")
 
 test_check("brapi")
