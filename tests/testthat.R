ae108efde80557ffda8889d5dd07907ffd690a03
c5sire <rsimon64@gmail.com>
2018-06-21 01:20:07 -0500

    re-grouping tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,3 +3,6 @@ library(brapi)
 
 ba_show_info(FALSE)
 test_check("brapi")
+
+# capture.output(testthat::test_dir('tests/testthat/sp', reporter = testthat::TapReporter), file = 'tests/testthat/sp/log-test.txt')
+# capture.output(testthat::test_dir('tests/testthat/ts', reporter = testthat::TapReporter), file = 'tests/testthat/ts/log-test.txt')
