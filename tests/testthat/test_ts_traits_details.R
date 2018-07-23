7ac0ab87a6c62881e38dfe458b761fb2af6a199e
Reinhard Simon <rsimon64@gmail.com>
2018-07-23 10:32:25 -0500

    test traits call against testserver. Closes #31

--- tests/testthat/test_ts_traits_details.R
+++ tests/testthat/test_ts_traits_details.R
@@ -1,17 +1,17 @@
-context("traits_details")
+context("ts traits_details")
 
-con <- ba_db()$sweetpotatobase
+con <- ba_db()$testserver
 
 test_that("Traits_details are present", {
 
-  res <- ba_traits_details(con = con)
-  expect_that(nrow(res) == 10, is_true())
+  res <- ba_traits_details(con = con, traitDbId = "1")
+  expect_that(nrow(res) == 3, is_true())
 
 })
 
 test_that("Traits_details are present", {
 
-  expect_error( ba_traits_details(con = con, rclass = "any"))
+  expect_error( ba_traits_details(con = con, traitDbId = "1",  rclass = "any"))
 
 })
 
