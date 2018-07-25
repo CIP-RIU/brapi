a36d1ba14c442421623295908d708c7a9961011a
Maikel Verouden <maikel.verouden@gmail.com>
2018-07-25 15:34:08 +0200

    made ba_studies_seasons compatible with BMS and updated documentation and tested against test-server and sweetpotatobase

--- R/ba_studies_seasons.R
+++ R/ba_studies_seasons.R
@@ -1,38 +1,53 @@
 #' ba_studies_seasons
 #'
-#' lists the studies seasons and years
-#'
-#' @note Tested against: sweetpotatobase
+#' Retrieve all seasons (or years) on a brapi server.
 #'
 #' @param con list, brapi connection object
-#' @param year integer
-#' @param page integer requested page number
-#' @param pageSize items per page
-#' @param rclass character; one of tibble (default), json, list or data.frame
-#' @import httr
-#' @author Reinhard Simon
-#' @return data.frame
-#' @example inst/examples/ex-ba_studies_seasons.R
-#' @references \href{https://github.com/plantbreeding/API/blob/master/Specification/Studies/ListSeasons.md}{github}
+#' @param year integer, filter by specified year; default: 0
+#' @param pageSize integer, items per page to be returned; default: 1000
+#' @param page integer, the requested page to be returned; default: 0 (1st page)
+#' @param rclass character, class of the object to be returned;  default: "tibble"
+#'               , possible other values: "json"/"list"/"data.frame"
+#'
+#' @return An object of class as defined by rclass containing all seasons (or
+#'         years).
+#'
+#' @note Tested against: sweetpotato, test-server
+#' @note BrAPI Version: 1.0, 1.1, 1.2
+#' @note BrAPI Status: active
+#'
+#' @author Reinhard Simon, Maikel Verouden
+#' @references \href{https://github.com/plantbreeding/API/blob/V1.2/Specification/Studies/Seasons_GET.md}{github}
 #' @family studies
 #' @family phenotyping
+#' @example inst/examples/ex-ba_studies_seasons.R
+#' @import httr
 #' @export
 ba_studies_seasons <- function(con = NULL,
                                year = 0,
-                               page = 0,
                                pageSize = 1000,
+                               page = 0,
                                rclass = "tibble") {
   ba_check(con = con, verbose = FALSE, brapi_calls = "seasons")
-  brp <- get_brapi(con = con)
   stopifnot(is.numeric(year))
   check_paging(pageSize = pageSize, page = page)
   check_rclass(rclass = rclass)
-  seasons_list <- paste0(brp, "seasons/?")
-  year <- ifelse(year != 0, paste0("year=", year, "&"), "")
-  page <- ifelse(is.numeric(page), paste0("page=", page, "&"), "")
-  pageSize <- ifelse(is.numeric(pageSize),
-                     paste0("pageSize=", pageSize, "&"), "")
-  seasons_list <- paste0(seasons_list, page, pageSize, year)
+  brp <- get_brapi(con = con)
+  seasons_list <- paste0(brp, "seasons?")
+  pyear <- ifelse(year != 0, paste0("year=", year, "&"), "")
+  ppage <- ifelse(is.numeric(page), paste0("page=", page, "&"), "")
+  ppageSize <- ifelse(is.numeric(pageSize), paste0("pageSize=",
+                                                   pageSize, "&"), "")
+  if (page == 0 & pageSize == 1000) {
+    ppage <- ""
+    ppageSize <- ""
+  }
+  seasons_list <- sub(pattern = "[/?&]$",
+                      replacement = "",
+                      x = paste0(seasons_list,
+                                 pyear,
+                                 ppageSize,
+                                 ppage))
   try({
     res <- brapiGET(url = seasons_list, con = con)
     res2 <- httr::content(x = res, as = "text", encoding = "UTF-8")
