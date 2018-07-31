b361d6b0e8c06c20d9a3f068a723bf9cd8cbfafb
Maikel Verouden <maikel.verouden@gmail.com>
2018-07-31 13:32:26 +0200

    added brapi-deprecated.R for documenting deprecated functions

---
+++ R/brapi-deprecated.R
@@ -0,0 +1,11 @@
+#' Deprecated functions in brapi
+#'
+#' These functions still work but will be removed (defunct) in a future version.
+#'
+#' \itemize{
+#'  \item \code{\link{ba_crops}}: This function is deprecated, and will
+#'  be removed in a future version of this package. Use \code{\link{ba_commoncropnames}} instead.
+#' }
+#'
+#' @name brapi-deprecated
+NULL
