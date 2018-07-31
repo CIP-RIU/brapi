7c53fd961668e51e7f1a3f26ef7cc01ee823405a
Reinhard Simon <rsimon64@gmail.com>
2018-07-31 11:15:40 -0500

    Fixed tests.

--- tests/testthat/test_ts_studies_search_get.R
+++ tests/testthat/test_ts_studies_search_get.R
@@ -5,22 +5,18 @@ con <- ba_db()$testserver
 
 
 test_that("Studies_search are present", {
-  # res <- ba_studies_search_post(con = con)
-  # expect_that(nrow(res) >= 1, is_true())
   res <- ba_studies_search_get(con = con)
-  expect_null(res)
+  expect_true(nrow(res) == 3)
 
 })
 
 test_that("Studies_search out formats work", {
 
-  expect_error({
-  res <- ba_studies_search_gett(con = con, rclass = "json")
+  res <- ba_studies_search_get(con = con, rclass = "json")
   expect_that("json" %in% class(res) , is_true())
   res <- ba_studies_search_get(con = con, rclass = "list")
   expect_that("list" %in% class(res) , is_true())
   res <- ba_studies_search_get(con = con, rclass = "data.frame")
   expect_that("data.frame" %in% class(res) , is_true())
-  })
 })
 
