812d9d8afabc415e4baf988391bca44218a69914
Reinhard Simon <rsimon64@gmail.com>
2018-08-02 14:14:39 -0500

    u

---
+++ R/template-depr_fun.R
@@ -0,0 +1,6 @@
+## template-depr_fun.R
+#'
+#' @name <%= fun %>-deprecated
+#' @usage <%= gsub("\n", "\n#' ", roxygen2:::wrapString(roxygen2:::function_usage(fun, formals(fun)))) %>
+#' @seealso \code{\link{brapi-deprecated}}
+#' @keywords internal
