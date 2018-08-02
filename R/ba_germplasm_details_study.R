9e80b0b445e537af834efefdebecc6f6d4b8efac
Maikel Verouden <maikel.verouden@gmail.com>
2018-08-02 16:39:29 +0200

    updated documentation and added deprecation of ba_germplasm_details_study to point to ba_studies_germplasm_details

--- R/ba_germplasm_details_study.R
+++ R/ba_germplasm_details_study.R
@@ -1,26 +1,36 @@
 #' ba_germplasm_details_study
 #'
-#' lists studies_germplasm available on a brapi server
+#' Gets germplasm details for a specific study available on a brapi server.
 #'
-#' @param con brapi connection object
-#' @param rclass character; default: tibble
-#' @param page integer; default 0
-#' @param pageSize integer; default 1000
-#' @param studyDbId character; default: ''
+#' @param con list, brapi connection object
+#' @param studyDbId character, the internal database identifier for a study of
+#'                  which the germplasm details are to be retrieved e.g. "1001";
+#'                  \strong{REQUIRED ARGUMENT} with default: ""
+#' @param pageSize integer, items per page to be returned; default: 1000
+#' @param page integer, the requested page to be returned; default: 0 (1st page)
+#' @param rclass character, class of the object to be returned;  default: "tibble"
+#'               , possible other values: "json"/"list"/"data.frame"
+#'
+#' @return An object of class as defined by rclass containing the germplasm
+#'         details for a requested study.
+#'
+#' @note Tested against: sweetpotatobase, test-server
+#' @note BrAPI Version: 1.0
+#' @note BrAPI Status: deprecated
 #'
 #' @author Reinhard Simon
-#' @references \url{https://github.com/plantbreeding/API/blob/master/Specification/Germplasm/GermplasmDetailsListByStudyDbId.md}
-#' @return rclass as defined
-#' @example inst/examples/ex-ba_germplasm_details_study.R
-#' @import tibble
+#' @references \href{https://github.com/plantbreeding/API/blob/V1.0/Specification/Germplasm/GermplasmDetailsListByStudyDbId.md}{github}
 #' @family germplasm
 #' @family brapicore
+#' @example inst/examples/ex-ba_germplasm_details_study.R
+#' @import tibble
 #' @export
 ba_germplasm_details_study <- function(con = NULL,
                                        studyDbId = "",
-                                       page = 0,
                                        pageSize = 1000,
+                                       page = 0,
                                        rclass = "tibble") {
+  .Deprecated(new = "ba_studies_germplasm_details")
   ba_check(con = con, verbose = FALSE, brapi_calls = "studies/id/germplasm")
   stopifnot(is.character(studyDbId))
   stopifnot(studyDbId != '')
@@ -29,12 +39,16 @@ ba_germplasm_details_study <- function(con = NULL,
   # fetch url of brapi implementation of the database
   brp <- get_brapi(con = con)
   # generate brapi call url
-  studies_germplasm_list <- paste0(brp, "studies/", studyDbId, "/germplasm/?")
-  page <- ifelse(is.numeric(page), paste0("page=", page), "")
+  studies_germplasm_list <- paste0(brp, "studies/", studyDbId, "/germplasm?")
   pageSize <- ifelse(is.numeric(pageSize), paste0("pageSize=",
                                                   pageSize, "&"), "")
+  page <- ifelse(is.numeric(page), paste0("page=", page, "&"), "")
   # modify brapi call url to include pagenation
-  studies_germplasm_list <- paste0(studies_germplasm_list, pageSize, page)
+  studies_germplasm_list <- sub("[/?&]$",
+                                "",
+                                paste0(studies_germplasm_list,
+                                       pageSize,
+                                       page))
   try({
     res <- brapiGET(url = studies_germplasm_list, con = con)
     res2 <- httr::content(x = res, as = "text", encoding = "UTF-8")
@@ -46,7 +60,6 @@ ba_germplasm_details_study <- function(con = NULL,
       out <- sgp2tbl(res = res2, rclass = rclass)
     }
     class(out) <- c(class(out), "ba_germplasm_details_study")
-
     show_metadata(res)
     return(out)
   })
