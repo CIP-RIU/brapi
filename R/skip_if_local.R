8c12af16d46010accffbcfaedc286853b1fb9372
Reinhard Simon <rsimon64@gmail.com>
2018-08-03 16:15:09 -0500

    messing with test conditions

--- R/skip_if_local.R
+++ R/skip_if_local.R
@@ -1,5 +1,5 @@
 skip_if_local <- function() {
-  if (!identical(Sys.getenv("NOT_CRAN"), "true")) {
+  if (as.integer(Sys.getenv("NUMBER_OF_PROCESSORS") < 13 )) {
     return(invisible(TRUE))
   }
   skip("if on local computer")
