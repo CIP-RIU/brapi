f2bb4b50dbfaca568e5fc98fcb9e2d2cca737349
Reinhard Simon <rsimon64@gmail.com>
2018-08-03 11:51:36 -0500

    re-organizing tests such that those dependent on internet are separated so testing is faster.

--- R/test_dbs.R
+++ R/test_dbs.R
@@ -1,4 +1,6 @@
 
-test_dbs <- function() {
-  testthat::test_dir("tests/sweetpotatobase")
+test_dbs <- function(resources = c("internet", "testserver","sweetpotatobase")) {
+  if("internet" %in% resources) testthat::test_dir("tests/internet")
+  if("testserver" %in% resources) testthat::test_dir("tests/testserver")
+  if("sweetpotatobase" %in% resources) testthat::test_dir("tests/sweetpotatobase")
 }
--- tests/sweetpotatobase/test_sp_trials.R
+++ tests/sweetpotatobase/test_sp_trials.R
@@ -1,26 +1,25 @@
-context("trials")
+context("sp trials")
 
-con <- ba_db()$testserver
+con <- ba_db()$sweetpotatobase
 
 test_that("Trials are present", {
 
-  res <- ba_trials(con = con, programDbId = "1")
+  res <- ba_trials(con = con)
   expect_that(nrow(res) > 1, is_true())
 
 })
 
 test_that("Param checks work", {
 
-  testthat::expect_message({
-    res <- ba_trials(con = con, programDbId = "")
-  })
+  res <- ba_trials(con = con, programDbId = "132")
+  expect_true(nrow(res) > 77)
 
 })
 
 
 test_that("Output is transformed", {
 
-  res <- ba_trials(con = con, programDbId = "1", rclass = "json")
+  res <- ba_trials(con = con, programDbId = "132", rclass = "json")
   expect_that("json" %in%  class(res), is_true())
 
 })
