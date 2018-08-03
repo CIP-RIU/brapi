2710f02388672820d7c0c088b409e88fa1eced4e
Reinhard Simon <rsimon64@gmail.com>
2018-08-03 16:27:08 -0500

    exposing skip function to avoid 'note' R CMD check.

--- R/skip_if_local.R
+++ R/skip_if_local.R
@@ -1,3 +1,12 @@
+
+#' skip_if_local
+#'
+#' A helper function to minimize time consuming testing against databases locally.
+#'
+#' Only used within formal tests of the package.
+#'
+#' @return boolean
+#' @export
 skip_if_local <- function() {
   if (as.integer(Sys.getenv("NUMBER_OF_PROCESSORS") < 13 )) {
     return(invisible(TRUE))
