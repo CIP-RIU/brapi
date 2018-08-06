a98c7f402c543c48fa1ea4ec59fa12b5b7a96e3b
Maikel Verouden <maikel.verouden@gmail.com>
2018-08-06 14:28:51 +0200

    further improved readability

--- R/test_dbs.R
+++ R/test_dbs.R
@@ -1,10 +1,13 @@
-
-test_dbs <- function(resources = c(
-  "internet",
-  "testserver",
-  "sweetpotatobase"
-  )) {
-  if("internet" %in% resources) testthat::test_dir("tests/internet")
-  if("testserver" %in% resources) testthat::test_dir("tests/testserver")
-  if("sweetpotatobase" %in% resources) testthat::test_dir("tests/sweetpotatobase")
+test_dbs <- function(resources = c("internet",
+                                   "testserver",
+                                   "sweetpotatobase")) {
+  if ("internet" %in% resources) {
+    testthat::test_dir("tests/internet")
+  }
+  if ("testserver" %in% resources) {
+    testthat::test_dir("tests/testserver")
+  }
+  if ("sweetpotatobase" %in% resources) {
+    testthat::test_dir("tests/sweetpotatobase")
+  }
 }
