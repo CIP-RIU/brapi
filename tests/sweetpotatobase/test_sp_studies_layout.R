c09cb6e10cc8af4d7a12f728885b2fe5fe50763c
Maikel Verouden <maikel.verouden@gmail.com>
2018-08-07 13:33:39 +0200

    updated test_sp_studies_layout.R with a good example

--- tests/sweetpotatobase/test_sp_studies_layout.R
+++ tests/sweetpotatobase/test_sp_studies_layout.R
@@ -1,18 +1,18 @@
 context("studies_layout")
 
-con <- ba_db()$testserver
+con <- ba_db()$sweetpotatobase
 
 test_that("Studies_layout are present", {
-
-  res <- ba_studies_layout(con = con, studyDbId = "1001")
-  expect_that(nrow(res) >= 4, is_true())
+  #!! sweetpotatobase has default pageSize =10 !!
+  res <- ba_studies_layout(con = con, studyDbId = "1292", pageSize = 28, page = 0)
+  expect_that(nrow(res) >= 28, is_true())
 
 })
 
 
 test_that("Output is transformed", {
 
-  res <- ba_studies_layout(con = con, studyDbId = "1001", rclass = "json")
+  res <- ba_studies_layout(con = con, studyDbId = "1292", rclass = "json")
   expect_that("json" %in%  class(res), is_true())
 
 })
