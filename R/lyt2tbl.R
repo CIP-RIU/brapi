5d7826ed3018bcf5ac941387806a4a9db83f01ca
Maikel Verouden <maikel.verouden@gmail.com>
2018-08-07 13:32:42 +0200

    fixed lyt2tbl function to handle additional data with arrays e.g. sweetpotatobase has additionalInfo on "plantNames": [] in the JSON response

--- R/lyt2tbl.R
+++ R/lyt2tbl.R
@@ -1,7 +1,26 @@
 lyt2tbl <- function(res, rclass) {
   lst <- jsonlite::fromJSON(txt = res)
-  dat <- jsonlite::toJSON(x = lst$result$data)
-  df <- jsonlite::fromJSON(txt = dat, simplifyDataFrame = TRUE, flatten = TRUE)
+  datJSON <- jsonlite::toJSON(x = lst$result$data)
+  df <- jsonlite::fromJSON(txt = datJSON)
+  # split off additionalInfo from df
+  dfAddInf <- df[["additionalInfo"]]
+  df[["additionalInfo"]] <- NULL
+  if (length(dfAddInf) == 0) {
+    df <- df
+  } else {
+    dfAddInf <- as.data.frame(lapply(X = dfAddInf,
+                                     FUN = function(x) {
+                                       if (class(x) == "list") {
+                                         x <- sapply(X = x ,
+                                                     FUN = paste0,
+                                                     collapse = "; ")
+                                       } else {
+                                         x <- x
+                                       }
+                                     }),
+                              stringsAsFactors = FALSE)
+    df <- cbind(df, dfAddInf)
+  }
   if (rclass == "tibble") {
     df <- tibble::as_tibble(x = df)
   }
