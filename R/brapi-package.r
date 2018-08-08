7fbbb796292e919324a0a3393039b0781748e5d4
Maikel Verouden <maikel.verouden@gmail.com>
2018-08-08 15:07:59 +0200

    updated layout brapi-package.R

--- R/brapi-package.r
+++ R/brapi-package.r
@@ -1,10 +1,13 @@
-#' brapi package.
+#' @title brapi Package.
 #'
-#' BrAPI (www.brapi.org) is an initiative to facilitate data exchange between databases dedicated to support plant breeding.
-#' The specific objective is to provide an Application Programming Interface (API) such that data can be accessed
-#' programmatically using a RESTful approach. The brapi R package accesses the API and transforms the data responses
-#' into tables. This project is simply meant as a container to organize publications.
+#' @description BrAPI (www.brapi.org) is an initiative to facilitate data
+#'              exchange between databases dedicated to support plant breeding.
+#'              The specific objective is to provide an Application Programming
+#'              Interface (API) such that data can be accessed programmatically
+#'              using a RESTful approach. The brapi R package accesses the API
+#'              and transforms the data responses into tables. This project is
+#'              simply meant as a container to organize publications.
 #'
-#' @name brapi
+#' @name brapi-package
 #' @docType package
 NULL
