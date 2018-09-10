cc60df532ddcc2fcbe5a10067576d40ae5198358
c5sire <rsimon64@gmail.com>
2018-09-10 15:57:00 -0500

    review

--- R/ba_studies_studytypes.R
+++ R/ba_studies_studytypes.R
@@ -2,20 +2,24 @@
 #'
 #' lists studies_studytypes available on a brapi server
 #'
-#' @note Tested against: sweetpotatobase
-#'
 #' @param con list, brapi connection object
 #' @param page integer; default 0
 #' @param pageSize integer; default 1000
 #' @param rclass character; default: tibble
+#' @return rclass as defined
+#'
+#' @note Tested against: sweetpotatobase, testserver
+#' @note BrAPI Version: 1.1, 1.2
+#' @note BrAPI Status: active
 #'
 #' @author Reinhard Simon
 #' @references \href{https://github.com/plantbreeding/API/blob/master/Specification/Studies/ListStudyTypes.md}{github}
-#' @return rclass as defined
-#' @example inst/examples/ex-ba_studies_studytypes.R
-#' @import tibble
 #' @family studies
 #' @family phenotyping
+#'
+#' @example inst/examples/ex-ba_studies_studytypes.R
+#' @import tibble
+
 #' @export
 ba_studies_studytypes <- function(con = NULL,
                                   page = 0,
@@ -26,12 +30,22 @@ ba_studies_studytypes <- function(con = NULL,
   check_rclass(rclass = rclass)
   brp <- get_brapi(con = con)
   pstudyTypes <- paste0(brp, "studytypes/?")
-  page <- ifelse(page < 0, paste0("page=", page, "&"), "")
-  pageSize <- ifelse(pageSize < 0,
-                     paste0("pageSize=", pageSize, "&"), "")
-  pstudyTypes <- paste0(pstudyTypes, pageSize, page)
+  ppageSize <- ifelse(is.numeric(pageSize), paste0("pageSize=",
+                                                   pageSize, "&"), "")
+  ppage <- ifelse(is.numeric(page), paste0("page=", page, "&"), "")
+  if (page == 0 & pageSize == 1000) {
+    ppage <- ""
+    ppageSize <- ""
+  }
+  # modify brapi call url to include pagination
+  callurl <- sub(pattern = "[/?&]$",
+                 replacement = "",
+                 x = paste0(pstudyTypes,
+                            ppageSize,
+                            ppage))
+
   try({
-    res <- brapiGET(url = pstudyTypes, con = con)
+    res <- brapiGET(url = callurl, con = con)
     res2 <- httr::content(x = res, as = "text", encoding = "UTF-8")
     out <- dat2tbl(res = res2, rclass = rclass)
     class(out) <- c(class(out), "ba_studies_studytypes")
--- tests/testserver/test_ts_observationvariables_search.R
+++ tests/testserver/test_ts_observationvariables_search.R
@@ -7,7 +7,7 @@ test_that("observationvariables_search results are present", {
 
   res <- ba_observationvariables_search(con = con, pageSize = 3)
   expect_that(nrow(res) == 3, is_true())
-  expect_that(ncol(res) >= 40, is_true())
+  expect_that(ncol(res) >= 36, is_true())
 
 })
 
