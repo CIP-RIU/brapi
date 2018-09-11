0c2718af7a1ea132d2cc7222d16343d3c8b218f3
c5sire <rsimon64@gmail.com>
2018-09-11 09:36:51 -0500

    revision

--- R/ba_studies_observationlevels.R
+++ R/ba_studies_observationlevels.R
@@ -2,27 +2,49 @@
 #'
 #' Call to retrieve the list of supported observation levels.
 #'
-#' Observation levels indicate the granularity level at which the measurements
+#' @param con brapi connection object
+#' @param pageSize integer, items per page to be returned; default: 1000
+#' @param page integer, the requested page to be returned; default: 0 (1st page)
+#' @param rclass character; default is 'tibble'
+#'
+#' @details Observation levels indicate the granularity level at which the measurements
 #' are taken. The values are used to supply the observationLevel parameter in
 #' the observation unit details call.
 #'
+#' @return An object of class as defined by rclass containing the details of the
+#'         measured observation variables for a requested study.
 #'
-#' @param con brapi connection object
-#' @param rclass character; default is FALSE; whether to display the raw list object or not
-#' @author Reinhard Simon
-#' @return a vector of crop names or NULL
-#' @example inst/examples/ex-ba_studies_observationlevels.R
+#' @note Tested against: sweetpotatobase, test-server
+#' @note BrAPI Version: 1.1, 1.2
+#' @note BrAPI Status: active
+#'
+#' @author Reinhard Simon, Maikel Verouden
+#'
+#' @references \href{https://github.com/plantbreeding/API/blob/V1.2/Specification/Studies/ObservationLevels_GET.md}{github}
 #' @family studies
 #' @family phenotyping
-#' @references \href{https://github.com/plantbreeding/API/blob/master/Specification/Studies/ListObservationLevels.md}{github}
+#' @example inst/examples/ex-ba_studies_observationlevels.R
 #' @export
 ba_studies_observationlevels <- function(con = NULL,
-                                         rclass = "vector") {
-  ba_check(con = con, verbose = FALSE, brapi_calls = "observationLevels")
+                                         pageSize = 1000,
+                                         page = 0,
+                                         rclass = "tibble") {
+  ba_check(con = con, verbose = FALSE, brapi_calls = "observationlevels")
   check_rclass(rclass = rclass)
-  observationLevels_List <- paste0(get_brapi(con = con), "observationLevels")
+  check_paging(pageSize = pageSize, page = page)
+  observationLevels_List <- paste0(get_brapi(con = con), "observationlevels")
+  ppageSize <- ifelse(is.numeric(pageSize), paste0("pageSize=",
+                                                   pageSize, "&"), "")
+  ppage <- ifelse(is.numeric(page), paste0("page=", page, "&"), "")
+
+  callurl <- sub(pattern = "[/?&]$",
+                 replacement = "",
+                 x = paste0(observationLevels_List,
+                            ppageSize,
+                            ppage))
+
   try({
-    res <- brapiGET(url = observationLevels_List, con = con)
+    res <- brapiGET(url = callurl, con = con)
     res2 <- httr::content(x = res, as = "text", encoding = "UTF-8")
     out <- dat2tbl(res = res2, rclass = rclass)
     class(out) <- c(class(out), "ba_studies_observationlevels")
