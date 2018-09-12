8618bd04961062c384377b3f04a78412ffbf9796
Verouden, Maikel <m.p.h.verouden@gmail.com>
2018-09-12 16:03:10 -0500

    updated ba_studies_details, ba_studies_table

--- R/ba_studies_details.R
+++ R/ba_studies_details.R
@@ -31,7 +31,8 @@
 #' @export
 ba_studies_details <- function(con = NULL,
                                studyDbId = "",
-                               rclass = c("tibble", "data.frame", "list", "json")) {
+                               rclass = c("tibble", "data.frame",
+                                          "list", "json")) {
   ba_check(con = con, verbose = FALSE, brapi_calls = "studies/id")
   studyDbId <- match_req(studyDbId)
   rclass <- match.arg(rclass)
