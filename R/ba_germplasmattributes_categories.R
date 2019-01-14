536856dd625e0a8f21dfb7a4fb984b82785bc95b
mverouden <maikel.verouden@gmail.com>
2019-01-14 14:53:01 +0100

    updated code and documentation ba_germplasmattributes_categories

--- R/ba_germplasmattributes_categories.R
+++ R/ba_germplasmattributes_categories.R
@@ -1,15 +1,21 @@
 #' ba_germplasmattributes_categories
 #'
-#' attibutes call.
+#' Retrieve available germplasm attribute categories.
 #'
 #' @param con brapi connection object
-#' @param page integer; default 0
-#' @param pageSize integer; default 10
-#' @param rclass character; default: tibble
+#' @param pageSize integer, items per page to be returned; default: 1000
+#' @param page integer, the requested page to be returned; default: 0 (1st page)
+#' @param rclass character, class of the object to be returned;  default: "tibble"
+#'               , possible other values: "json"/"list"/"vector"/"data.frame"
 #'
-#' @return rclass as set by parameter
+#' @return An object of class as defined by rclass containing the available
+#'         germplasm attribute categories.
 #'
-#' @author Reinhard Simon
+#' @note Tested against: test-server, sweetpotatobsse
+#' @note BrAPI Version: 1.0, 1.1, 1.2
+#' @note BrAPI Status: active
+#'
+#' @author Reinhard Simon, Maikel Verouden
 #' @references \href{https://github.com/plantbreeding/API/blob/V1.2/Specification/GermplasmAttributes/Attributes_Categories_GET.md}{github}
 #'
 #' @family germplasmattributes
@@ -22,20 +28,22 @@
 ba_germplasmattributes_categories <- function(con = NULL,
                                               pageSize = 1000,
                                               page = 0,
-                                              rclass = c("tibble", "data.frame", "list", "json")) {
+                                              rclass = c("tibble", "data.frame",
+                                                          "list", "json")) {
   ba_check(con = con, verbose = FALSE)
-  brp <- get_brapi(con = con) %>% paste0("attributes/categories")
-  callurl <- get_endpoint(brp,
-                          pageSize = pageSize,
-                          page = page
-                          )
   rclass <- match_req(rclass)
+
+  brp <- get_brapi(con = con) %>% paste0("attributes/categories")
+  callurl <- get_endpoint(pointbase = brp,
+                          pageSize = pageSize,
+                          page = page)
+
   try({
-    res <- brapiGET(url = callurl, con = con)
-    res2 <- httr::content(x = res, as = "text", encoding = "UTF-8")
-    out <- dat2tbl(res = res2, rclass = rclass)
+    resp <- brapiGET(url = callurl, con = con)
+    cont <- httr::content(x = resp, as = "text", encoding = "UTF-8")
+    out <- dat2tbl(res = cont, rclass = rclass)
     class(out) <- c(class(out), "ba_germplasmattributes_categories")
-    show_metadata(res)
+    show_metadata(resp)
     return(out)
   })
 }
