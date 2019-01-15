21e7468caa3e9da4466dc7a6e06c58e30a626c5e
mverouden <maikel.verouden@gmail.com>
2019-01-15 15:11:55 +0100

    updated code and documentation ba_markers_details

--- R/ba_markers_details.R
+++ R/ba_markers_details.R
@@ -1,18 +1,26 @@
 #' ba_markers_details
 #'
-#' Lists markers as result of a search.
+#' Retrieve marker details.
 #'
-#' @param con brapi connection object
-#' @param markerDbId character; marker id; \strong{REQUIRED ARGUMENT} with default: ""
-#' @param rclass character; default: tibble
+#' @param con list, brapi connection object
+#' @param markerDbId character, the internal database identifier for a marker of
+#'                   which the marker details are to be retrieved e.g. "1185";
+#'                   \strong{REQUIRED ARGUMENT} with default: ""
+#' @param rclass character, class of the object to be returned;  default: "tibble"
+#'               , possible other values: "data.frame"/"list"/"json"
 #'
-#' @return data.frame
+#' @return An object of class as defined by rclass containing the marker details.
 #'
-#' @author Reinhard Simon
+#' @note Tested against: test-server
+#' @note BrAPI Version: 1.0, 1.1, 1.2
+#' @note BrAPI Status: active
+#'
+#' @author Reinhard Simon, Maikel
 #' @references \href{https://github.com/plantbreeding/API/blob/V1.2/Specification/Markers/MarkerDetailsByMarkerDbId.md}{github}
 #'
 #' @family markers
 #' @family genotyping
+#'
 #' @example inst/examples/ex-ba_markers_details.R
 #'
 #' @import httr
@@ -27,15 +35,16 @@ ba_markers_details <- function(con = NULL,
   check_character(markerDbId)
   check_req(markerDbId)
   rclass <- match.arg(rclass)
+
   callurl <- get_brapi(con = con) %>% paste0("markers/", markerDbId)
 
   try({
-    res <- brapiGET(url = callurl, con = con)
-    res2 <- httr::content(x = res, as = "text", encoding = "UTF-8")
-    out <- dat2tbl(res = res2 , rclass = rclass, result_level = "result")
+    resp <- brapiGET(url = callurl, con = con)
+    cont <- httr::content(x = resp, as = "text", encoding = "UTF-8")
+    out <- dat2tbl(res = cont, rclass = rclass, result_level = "result")
 
     class(out) <- c(class(out), "ba_markers_details")
-    show_metadata(res)
+    show_metadata(resp)
     return(out)
   })
 }
