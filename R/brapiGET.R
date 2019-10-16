5f2a5caa48d72e2412ead128b9143cc1882a060c
mverouden <maikel.verouden@gmail.com>
2019-10-16 17:51:00 +0200

    restructuring ba_db.R and brapiGET.R

--- R/brapiGET.R
+++ R/brapiGET.R
@@ -2,10 +2,9 @@ brapiGET <- function(url, format = "json", con = NULL) {
   ba_message(msg = paste0("URL call was: ", url, "\n"))
   ba_message(msg = paste0("Waiting for response from server: ...\n"))
 
-  res <- httr::GET(url = url, httr::timeout(25),
-        httr::add_headers("Authorization" =
-                            paste("Bearer", con$token)))
-
+  res <- httr::GET(url = url,
+                   httr::timeout(25),
+                   httr::add_headers("Authorization" = paste("Bearer", con$token)))
 
   txt <- ifelse(res$status == 200, " ok!", " problem!")
   ba_message(msg = paste0("Server status: ", txt, "\n"))
